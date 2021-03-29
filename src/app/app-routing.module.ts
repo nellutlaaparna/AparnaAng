396dbed9c423ba42cb378e152cb2cc49c004cd66
nellutlaaparna <nellutlaparna7481@gmail.com>
2021-03-30 00:19:02 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ServicesComponent } from './services/services.component';
+
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
     {path:"contact",component:ContactComponent},
-    {path:"about",component:AboutComponent}
+    {path:"about",component:AboutComponent},
+    {path:"services",component:ServicesComponent},
+    {path:"signup",component:SignupComponent},
+    {path:"login",component:LoginComponent},
+    
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    ServicesComponent,
+    
+    SigninComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
